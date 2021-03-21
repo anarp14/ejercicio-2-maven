1d9de4855d3c04a5999ce4c6ce4921402d4aadca
Ana Rodriguez <anamaria.rodriguez@iesdonana.org>
2021-03-21 23:49:30 +0100

    commit inicial

---
+++ Rodriguez/src/test/java/arp/endes/AppTest.java
@@ -0,0 +1,20 @@
+package arp.endes;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
